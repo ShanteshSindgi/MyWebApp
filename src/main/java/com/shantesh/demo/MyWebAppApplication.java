b8db6d796b8cea32715e7c35af84ad1a60bba6a8
Shantesh Sindgi <sindgishantesh@gmail.com>
2021-03-23 00:35:25 +0530

    ASd

--- src/main/java/com/shantesh/demo/MyWebAppApplication.java
+++ src/main/java/com/shantesh/demo/MyWebAppApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
 public class MyWebAppApplication extends SpringBootServletInitializer {
-
+//d
 	public static void main(String[] args) {
 		SpringApplication.run(MyWebAppApplication.class, args);
 	}
