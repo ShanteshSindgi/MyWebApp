45cca34d3b61840a6f18c2ba2852cde979251901
Shantesh Sindgi <sindgishantesh@gmail.com>
2021-03-23 00:32:13 +0530

    ASd

--- src/main/java/com/shantesh/demo/MyWebAppApplication.java
+++ src/main/java/com/shantesh/demo/MyWebAppApplication.java
@@ -2,12 +2,17 @@ package com.shantesh.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MyWebAppApplication {
+public class MyWebAppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyWebAppApplication.class, args);
 	}
-
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	        return builder.sources(MyWebAppApplication.class);
+	    }
 }
