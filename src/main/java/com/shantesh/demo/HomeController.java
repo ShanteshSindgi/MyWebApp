edd7325ef94047fb97c79f972cc83d7a4f8f90a8
Shantesh Sindgi <sindgishantesh@gmail.com>
2021-03-22 14:51:49 +0530

    first

---
+++ src/main/java/com/shantesh/demo/HomeController.java
@@ -0,0 +1,21 @@
+package com.shantesh.demo;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	 
+	
+	@RequestMapping("home")
+	public String home(HttpServletRequest req)
+	{
+		String name=req.getParameter("name");
+		System.out.println("In home"+ name);
+
+		return "home";
+	}
+}
